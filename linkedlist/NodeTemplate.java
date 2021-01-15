cf283f90d5c348bebc87347ee513b3b93ea228f8
Anurag Anand <anuraganand789@hotmail.com>
2021-01-15 21:07:29 +0530

    code to start LinkedLIst coding

---
+++ linkedlist/NodeTemplate.java
@@ -0,0 +1,58 @@
+    private static void printALinkedList(Node node){
+        while(node != null) {
+	    System.out.format("%d -> ", node.nextNode);
+	    node = node.nextNode;
+	}
+
+	System.out.println(node);
+    }
+    private static int lengthOfLinkedList(Node headOfTheLinkedList){
+        int numberOfNodes  = 0;
+	while(headOfTheLinkedList != null) { 
+	    headOfTheLinkedList = headOfTheLinkedList.nextNode;
+	    ++numberOfNodes; 
+	}
+
+	return numberOfNodes;
+    }
+    private static Node rotate(final Node headOfTheLinkedList, final int times){
+	if(headOfTheLinkedList == null) return null;
+        final int lengthOfTheLinkedList = lengthOfLinkedList(headOfTheLinkedList);
+        
+	int noOfTimeToBeRotated = lengthOfTheLinkedList - (times % lengthOfTheLinkedList);
+
+	Node currentNode = headOfTheLinkedList;
+
+	while(noOfTimeToBeRotated > 0){
+	    currentNode = currentNode.nextNode;
+	    --noOfTimeToBeRotated;
+	}
+
+	Node newHead = currentNode.nextNode;
+        currentNode.nextNode = null;
+	
+	Node iterator = newHead;
+	while(iterator.nextNode != null) iterator = iterator.nextNode;
+        
+	iterator.nextNode = headOfTheLinkedList;
+    }
+    private static class Node{
+        private int data;
+        private Node nextNode;
+    
+        private Node(final int data){
+            this(data, null);
+        }
+    
+        private Node(final int data, final Node nextNode){
+            this.data = data;
+    	    this.nextNode = nextNode;
+        }
+    }
+
+    public static void main(final String[] commandLineArguments){
+	Node node3 = new Node(3);
+        Node node2 = new Node(2, node3);
+        Node node1 = new Node(1, node2);
+        Node head  = new Node(0, node1);
+    }
